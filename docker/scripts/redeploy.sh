e22c9245e0eb3cca9a399d8e6d2d32b37ebe705f
Muhammad Mega Nugraha <monmega110@gmail.com>
2025-11-29 19:31:42 +0700

    deleted all necessary docker buildscripts

--- docker/scripts/redeploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-echo "Redeploy hook placeholder"
-# Add redeploy steps here (clear caches, restart services, warm caches)
-php artisan config:cache || true
-php artisan route:cache || true
-php artisan view:cache || true
